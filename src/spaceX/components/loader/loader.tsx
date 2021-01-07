8cf67754b666a0ca057595e69ffd5db7c1176e13
asadalibest1 <asadalibest1@gmail.com>
2021-01-07 12:02:51 +0500

    commit

--- src/spaceX/components/loader/loader.tsx
+++ src/spaceX/components/loader/loader.tsx
@@ -5,18 +5,18 @@ export default function Loader() {
     return (
         <div>
             <div className="lds-spinner">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div key="1"></div>
+                <div key="2"></div>
+                <div key="3"></div>
+                <div key="4"></div>
+                <div key="5"></div>
+                <div key="6"></div>
+                <div key="7"></div>
+                <div key="8"></div>
+                <div key="9"></div>
+                <div key="10"></div>
+                <div key="11"></div>
+                <div key="12"></div>
             </div>
         </div>
     )
