c2e09a698e3e68a976ded8648f6e55576ac206fe
Asad Ali <saifalikorai@gmail.com>
2021-03-24 15:42:18 +0500

    commit

--- src/spaceX/components/Home/components/StageTwo/StageTwo.tsx
+++ src/spaceX/components/Home/components/StageTwo/StageTwo.tsx
@@ -5,7 +5,7 @@ import Loader from "./loader";
 
 import "./StageTwo.css"
 
-export default function StageTwo({getId}) {
+export default function StageTwo({ getId }) {
     const { data, loading } = useMissionInfoQuery();
 
     if (loading)
@@ -13,41 +13,44 @@ export default function StageTwo({getId}) {
 
     return (
         <div className="stage-second">
-        <div className="stage-second-inner-div">
-            <h1 className="heading-Past-Missions">Past Missions</h1>
-          
-          
-                        <div className="mission-view">
-                        <img src={data.launches[16].links.flickr_images} alt="picter" />
-                        <h2>{data.launches[16].mission_name}</h2>
-                        <label>Success</label>
-                            <Link to="/missionDetails" className="Link">
-                                <button onClick={()=>{getId(16)}}>Mission Details</button>
-                            </Link>
-                        </div>
-                        
-                        <div className="mission-view">
-                        <img src={data.launches[25].links.flickr_images} alt="picter" />
-                        <h2>{data.launches[25].mission_name}</h2>
-                        <label>Success</label>
-                            <Link to="/missionDetails" className="Link">
-                                <button onClick={()=>{getId(25)}}>Mission Details</button>
-                            </Link>
-                        </div>
+            <div className="stage-second-inner-div">
+                <h1 className="heading-Past-Missions">Past Missions</h1>
 
-                        <div className="mission-view">
-                        <img src={data.launches[31].links.flickr_images} alt="picter" />
-                        <h2>{data.launches[31].mission_name}</h2>
-                        <label>Success</label>
-                            <Link to="/missionDetails" className="Link">
-                                <button onClick={()=>{getId(31)}}>Mission Details</button>
-                            </Link>
-                        </div>
 
-                        <Link to="/missions">
+                <div className="mission-view">
+                    <img src={data.launches[16].links.flickr_images} alt="picter" />
+                    <h2>{data.launches[16].mission_name}</h2>
+                    <label>Success</label>
+                    <Link to="/missionDetails" className="Link">
+                        <button onClick={() => { getId(16) }}>Mission Details</button>
+                    </Link>
+                </div>
+
+                <div className="mission-view">
+                    <img src={data.launches[25].links.flickr_images} alt="picter" />
+                    <h2>{data.launches[25].mission_name}</h2>
+                    <label>Success</label>
+                    <Link to="/missionDetails" className="Link">
+                        <button onClick={() => { getId(25) }}>Mission Details</button>
+                    </Link>
+                </div>
+
+                <div className="mission-view">
+                    <img src={data.launches[31].links.flickr_images} alt="picter" />
+                    <h2>{data.launches[31].mission_name}</h2>
+                    <label>Success</label>
+                    <Link to="/missionDetails" className="Link">
+                        <button onClick={() => { getId(31) }}>Mission Details</button>
+                    </Link>
+                </div>
+
+
+                <div className="Show-All-Missions-div">
+                    <Link to="/missions">
                         <button className="Show-All-Missions">Show All Missions</button>
-                        </Link>
-        </div>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
--- src/spaceX/components/Home/components/StageTwo/loader.tsx
+++ src/spaceX/components/Home/components/StageTwo/loader.tsx
@@ -7,30 +7,33 @@ export default function Loader() {
             <div className="stage-second-inner-div">
                 <h1 className="heading-Past-Missions">Past Missions</h1>
                 <div className="mission-view" >
-                        <div className="noImage loadingImage">
-                            <Spinner />
-                        </div>
-                        <h2 className="loading-h2"></h2>
-                        <label className="loading-label"></label>
-                        <span className="loading-button"></span>
+                    <div className="noImage loadingImage">
+                        <Spinner />
+                    </div>
+                    <h2 className="loading-h2"></h2>
+                    <label className="loading-label"></label>
+                    <span className="loading-button"></span>
                 </div>
                 <div className="mission-view" >
-                        <div className="noImage loadingImage">
-                            <Spinner />
-                        </div>
-                        <h2 className="loading-h2"></h2>
-                        <label className="loading-label"></label>
-                        <span className="loading-button"></span>
+                    <div className="noImage loadingImage">
+                        <Spinner />
+                    </div>
+                    <h2 className="loading-h2"></h2>
+                    <label className="loading-label"></label>
+                    <span className="loading-button"></span>
                 </div>
                 <div className="mission-view" >
-                        <div className="noImage loadingImage">
-                            <Spinner />
-                        </div>
-                        <h2 className="loading-h2"></h2>
-                        <label className="loading-label"></label>
-                        <span className="loading-button"></span>
+                    <div className="noImage loadingImage">
+                        <Spinner />
+                    </div>
+                    <h2 className="loading-h2"></h2>
+                    <label className="loading-label"></label>
+                    <span className="loading-button"></span>
                 </div>
+                <div className="Show-All-Missions-div">
                     <button className="Show-All-Missions">Show All Missions</button>
+
+                </div>
             </div>
         </div>
     )
